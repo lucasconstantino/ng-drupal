4871792918633af876cd0ff252147fabe5ba1273
Lucas Constantino <lucas@taller.net.br>
2014-01-29 18:57:39 -0200

    Created sample node reader application.

---
+++ demo/node-reader/app/app.js
@@ -0,0 +1,44 @@
+
+/**
+ * ------------------------------------------------------------------------
+ * Demo - Drupal Node-Reader via AngularJS
+ * ------------------------------------------------------------------------
+ * This demo application makes use of the drupal-services angular module
+ * to consume node data and show as a list.
+ */
+
+angular.module('node-reader', ['drupal.services'])
+  
+  /**
+   * DrupalServices configuration.
+   */
+  .config(function (DrupalServicesProvider) {
+
+    // The base url set bellow should point to your Drupal Services
+    // endpoint. To understand how it works, please refeer to
+    // (https://drupal.org/node/783460).
+    
+    // If you are running the Drupal in a different domain from the
+    // AngularJS application (as is my case), you have to allow CORS on
+    // the Drupal side. You can do it thorught the CORS module of Drupal,
+    // which you can find at (https://drupal.org/project/cors).
+    
+    DrupalServicesProvider.setBaseUrl('http://contrib.drupal/api');
+
+  })
+
+  /**
+   * Create a factory only for managing the nodes resource.
+   */
+  .factory('Nodes', function(DrupalServices) {
+
+    // "node" is a endpoint at Drupal that should return a collection of nodes.
+    return DrupalServices.all('node').getList().$object;
+  })
+
+  /**
+   * NodeReaderController
+   */
+  .controller('NodeReaderController', function ($scope, Nodes) {
+    $scope.nodes = Nodes;
+  });
